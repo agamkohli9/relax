4437992ad05165a3474c97df8a75cdf883e22c54
Agam Kohli <agamkohli9@gmail.com>
2022-12-09 17:18:29 -0500

    Common Subexpression Elimination

---
+++ test-agam/compile_small.py
@@ -0,0 +1,14 @@
+""" Compiles the model given in model.py """ 
+
+import tvm
+from tvm import relax, relay
+from small import SmallModel
+
+if __name__ == '__main__':
+    with open('small.relax', 'w') as f:
+        print(SmallModel, file=f)
+
+    optimized_mod = relax.transform.DeadCodeElimination()(SmallModel)
+
+    with open('small.optimized.relax', 'w') as f:
+        print(optimized_mod, file=f)
