439553fe9fe2b1f006f2caf58523773592c2b437
Agam Kohli <agamkohli9@gmail.com>
2022-12-09 19:23:12 -0500

    easier to read test case

--- test-agam/compile_small.py
+++ test-agam/compile_small.py
@@ -2,13 +2,41 @@
 
 import tvm
 from tvm import relax, relay
-from small import SmallModel
+from tvm.script import relax as R
+from tvm.script.parser.relax.entry import Tensor
+
+def run_opt_pass(expr, opt_pass):
+    assert isinstance(opt_pass, tvm.transform.Pass)
+    mod = tvm.IRModule.from_expr(expr)
+    mod = opt_pass(mod)
+    entry = mod["main"]
+    return entry if isinstance(expr, relay.Function) else entry.body
+
+def small_relay_model():
+    a = relay.const(69)
+    b = relay.const(69)
+    c = relay.add(a, b)
+    d = relay.add(a, b) # Should get removed
+    e = relay.add(c, d)
+    return e
+
+def small_model():
+    a = R.const(69)
+    b = R.const(69)
+    c = R.add(a, b)
+    d = R.add(a, b) # Should get removed
+    e = R.add(c, d)
+    return e
 
 if __name__ == '__main__':
-    with open('small.relax', 'w') as f:
-        print(SmallModel, file=f)
+    #mod = small_relay_model()
+    mod = small_model()
 
-    optimized_mod = relax.transform.CommonSubexpressionElimination()(SmallModel)
+    with open('small.relax', 'w') as f:
+        print(mod, file=f)
+
+    #optimized_mod = run_opt_pass(mod, relay.transform.EliminateCommonSubexpr())
+    optimized_mod = run_opt_pass(mod, relax.transform.CommonSubexpressionElimination())
 
     with open('small.optimized.relax', 'w') as f:
         print(optimized_mod, file=f)
--- test-agam/small.py
+++ test-agam/small.py
@@ -1,15 +1,17 @@
 import tvm
-from tvm import relax
-from tvm.script import relax as R
-from tvm.script.parser.relax.entry import Tensor
+from tvm import relay
+from tvm.ir.module import IRModule
+from tvm.script import tir as T
+import numpy as np
 
 @tvm.script.ir_module
 class SmallModel:
-    @R.function
-    def main(x: Tensor, y: Tensor):
-            a = R.const(12)
-            b = R.const(12)
-            c = R.add(b, b)
-            d = R.add(b, b) # = c
-            e = R.add(c, d)
-            return e
+    def main():
+        x = relay.var("x", shape=(1, 16))
+        y1 = relay.nn.relu(x)
+        y2 = relay.nn.relu(x)
+        y1 = relay.add(y1, relay.const(1.0, "float32"))
+        y2 = relay.add(y2, relay.const(1.0, "float32"))
+        y = relay.add(y1, y2)
+        f = relay.Function([x], y)
+        return f
