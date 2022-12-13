30c3c4b08bba3964fc6251dfb33747a497ac71e5
jfaro <jfaro@umich.edu>
2022-12-13 01:44:09 -0500

    Update gen_relax

--- demo/gen_relax_ir.py
+++ demo/gen_relax_ir.py
@@ -2,8 +2,7 @@ from definitions import RELAX_IR_DIR
 from relax.optimize import get_modules, optimize_and_save_model
 
 modules = get_modules()
-print("modules:", modules)
 
-for mod in modules:
-    print("compiling model ", mod[0])
-    optimize_and_save_model(RELAX_IR_DIR, mod[0], mod[1])
+for name, mod in modules:
+    print("compiling model ", name)
+    optimize_and_save_model(RELAX_IR_DIR, name, mod)
